7ed63d8b5b52be1428b160c3e7b117e76d62a955
Mitesh <rathodmeet9153@gmail.com>
2024-06-22 18:26:24 +0530

    Initial commit

---
+++ Sum.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class FirstClass {
+    public static void main(String args[]) {
+        // Input
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        // SUM
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
+
+        // // Variables
+        // // System.out.print("*\n**\n***\n****");
+        // int a = 10;
+        // int b = 5;
+        // int sum = a + b;
+        // System.out.println(sum);
+        // int diff = a - b;
+        // System.out.println(diff);
+        // int mul = a * b;
+        // System.out.println(mul);
+        // int ans = (a * b) / (a - b);
+        // System.out.println(ans);
+    }
+}
